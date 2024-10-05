6f9c6a3939ac177f3b4ceeec099ef40e756bc18b
ቆስጣ <graemewada999@gmail.com>
2024-10-05 11:58:13 -0600

    update LemLib

--- LLT4/src/main.cpp
+++ LLT4/src/main.cpp
@@ -2,21 +2,24 @@
 #include "lemlib/api.hpp" // IWYU pragma: keep
 
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
-pros::MotorGroup left_mg({-1, 3}, pros::MotorGearset::blue);    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({9, -17}, pros::MotorGearset::blue);
+pros::MotorGroup left_mg({-13, -12, 11}, pros::MotorGearset::blue);    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
+	pros::MotorGroup right_mg({16, 15, -14}, pros::MotorGearset::blue);
+    pros::Motor intake(17, pros::MotorGearset::blue);
 
-lemlib::Drivetrain drivetrain(&left_mg, &right_mg, 11.5, lemlib::Omniwheel::NEW_325, 480, 2);
+pros::ADIDigitalOut clampPistons ('H');
+//trackwidth 29cm = ~11.417in
+lemlib::Drivetrain drivetrain(&left_mg, &right_mg, 11.417, lemlib::Omniwheel::NEW_325, 450, 2);
 
 
 //odom
-// pros::Imu imu(10);
-// pros::Rotation rv(7); //vert
-// pros::Rotation rh(8); //horiz set to negative if reverse
+// pros::Imu imu(19);
+// // pros::Rotation rv(7); //vert
+// pros::Rotation rh(-6); //horiz set to negative if reverse
 
-// lemlib::TrackingWheel htw(&rh, 3.25, -5.25); // third value is tracking center offset
+// lemlib::TrackingWheel htw(&rh, lemlib::Omniwheel::NEW_275, 2.125); // third value is tracking center offset
 // lemlib::TrackingWheel vtw(&rv, 3.25, 5.25);
 
-// lemlib::OdomSensors sensors(&vtw, // vertical tracking wheel 1, set to null
+// lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to null
 //                             nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
 //                             &htw, // horizontal tracking wheel 1
 //                             nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
@@ -29,27 +32,28 @@ lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to null
                             nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
                             nullptr // inertial sensor
 );
+
 // lateral PID controller
-lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+lemlib::ControllerSettings lateral_controller(11, // proportional gain (kP)
                                               0, // integral gain (kI)
                                               3, // derivative gain (kD)
-                                              3, // anti windup
-                                              1, // small error range, in inches
-                                              100, // small error range timeout, in milliseconds
-                                              3, // large error range, in inches
-                                              500, // large error range timeout, in milliseconds
-                                              20 // maximum acceleration (slew)
+                                              0, // anti windup
+                                              0, // small error range, in inches
+                                              0, // small error range timeout, in milliseconds
+                                              0, // large error range, in inches
+                                              0, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
 );
 
 // angular PID controller
-lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+lemlib::ControllerSettings angular_controller(5, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              10, // derivative gain (kD)
-                                              3, // anti windup
-                                              1, // small error range, in degrees
-                                              100, // small error range timeout, in milliseconds
-                                              3, // large error range, in degrees
-                                              500, // large error range timeout, in milliseconds
+                                              12, // derivative gain (kD)
+                                              0, // anti windup
+                                              0, // small error range, in degrees
+                                              0, // small error range timeout, in milliseconds
+                                              0, // large error range, in degrees
+                                              0, // large error range timeout, in milliseconds
                                               0 // maximum acceleration (slew)
 );
 
@@ -100,8 +104,21 @@ void initialize() {
 	pros::lcd::set_text(1, "Hello PROS User!");
 
 	pros::lcd::register_btn1_cb(on_center_button);
+    chassis.calibrate();
+    // pros::Task screenTask([&]() {
+    //     while (true) {
+    //         // print robot location to the brain screen
+    //         pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+    //         pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+    //         pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+    //         // log position telemetry
+    //         lemlib::telemetrySink()->info("Chassis pose: {}", chassis.getPose());
+    //         // delay to save resources
+    //         pros::delay(50);
+    //     }
+    // });
 	// while (true){
-    //     //pros::lcd::print(1, "Rotation Sensor H: %i", rh.get_position());
+    //     pros::lcd::print(1, "Rotation Sensor H: %i", rh.get_position());
 	// 	//pros::lcd::print(0, "Rotation Sensor V: %i", rh.get_position());
     //     pros::delay(10); // delay to save resources. DO NOT REMOVE
 	// }
@@ -136,8 +153,14 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
+ASSET(path1_txt);
+
 void autonomous() {
 	chassis.setPose(0,0,0);
+    // turn to face heading 90 with a very long timeout
+    //chassis.turnToHeading(90, 100000);
+    chassis.follow(path1_txt, 4000, 15);
+
 }
 
 /**
@@ -158,9 +181,29 @@ void autonomous() {
 
 
     void opcontrol() {
+
+        bool clamp = false;
+        intake.set_voltage_limit(12000);
         // controller
         // loop to continuously update motors
         while (true) {
+            if(controller.get_digital_new_press(DIGITAL_DOWN)){
+                if(!clamp){
+                    clamp = true;
+                    clampPistons.set_value(true);
+                } else {
+                    clamp = false;
+                    clampPistons.set_value(false);
+                }
+            }
+
+            if(controller.get_digital(DIGITAL_R2)){
+                intake.move_voltage(12000);
+            } else if (controller.get_digital(DIGITAL_R1)){
+                intake.move_voltage(-12000);
+            } else {
+                intake.brake();
+            }
             // get joystick positions
             int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
             int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
